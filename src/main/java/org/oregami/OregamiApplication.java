ed46fdab3eb6424b9c318957a4979e1d9e17b378
Sebastian Eichholz <gene@kultpower.de>
2016-11-03 21:39:36 +0100

    initial commit

---
+++ src/main/java/org/oregami/OregamiApplication.java
@@ -0,0 +1,12 @@
+package org.oregami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OregamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OregamiApplication.class, args);
+	}
+}

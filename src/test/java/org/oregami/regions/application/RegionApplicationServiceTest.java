75e37cacc06008d39471404505a942e2c24d6a82
Sebastian Eichholz <gene@oregami.org>
2017-03-04 17:52:55 +0100

    Test fix

--- src/test/java/org/oregami/regions/application/RegionApplicationServiceTest.java
+++ src/test/java/org/oregami/regions/application/RegionApplicationServiceTest.java
@@ -5,16 +5,13 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.oregami.game.application.GameApplicationService;
 import org.oregami.regions.RRegionRepository;
+import org.oregami.regions.RegionId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-
-import static org.junit.Assert.*;
 
 /**
  * Created by sebastian on 28.02.17.
@@ -39,7 +36,7 @@ public class RegionApplicationServiceTest {
         String id = "GERMANY";
         int size = repository.findAll().size();
         CompletableFuture<Object> resultId = regionApplicationService.createNewRegion(id, true, false, "a nice country");
-        Assert.assertThat(resultId.get(), Matchers.equalTo(id));
+        Assert.assertThat(((RegionId)resultId.get()).getValue(), Matchers.equalTo(id));
         Assert.assertThat(repository.findAll().size(), Matchers.equalTo(size+1));
     }
 
